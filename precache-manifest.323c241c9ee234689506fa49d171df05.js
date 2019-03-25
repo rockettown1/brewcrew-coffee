42f41e88496bdf15af8933d35705dddcfc1cd19a
Dan Krishnan <dan.krishnan01@gmail.com>
2019-03-25 12:19:42 +0000

    Updates

---
+++ precache-manifest.323c241c9ee234689506fa49d171df05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56d51d65220119caf0e9",
+    "url": "/brewcrew-coffee/static/js/runtime~main.56d51d65.js"
+  },
+  {
+    "revision": "5242007be80306d037c3",
+    "url": "/brewcrew-coffee/static/js/main.5242007b.chunk.js"
+  },
+  {
+    "revision": "c3d76e8179fa7d39143c",
+    "url": "/brewcrew-coffee/static/js/1.c3d76e81.chunk.js"
+  },
+  {
+    "revision": "5242007be80306d037c3",
+    "url": "/brewcrew-coffee/static/css/main.afac3c8b.chunk.css"
+  },
+  {
+    "revision": "c3d76e8179fa7d39143c",
+    "url": "/brewcrew-coffee/static/css/1.662830b7.chunk.css"
+  },
+  {
+    "revision": "9b6d914910eb68bda96405b63c20174a",
+    "url": "/brewcrew-coffee/index.html"
+  }
+];
